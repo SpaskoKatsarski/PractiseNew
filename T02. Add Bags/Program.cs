8b3aa8c035af3e7872717934bb09c498129eb32d
Spasko Katsarski <patsokatsarski1@gmail.com>
2021-10-13 23:33:07 +0300

    Updated

---
+++ T02. Add Bags/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace T02._Add_Bags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceLaugageOver20Kilos = double.Parse(Console.ReadLine());
+            double lougageKilos = double.Parse(Console.ReadLine());
+            int daysTillTrip = int.Parse(Console.ReadLine());
+            int lougageCount = int.Parse(Console.ReadLine());
+
+            double lougagePrice = 0;
+
+            if (lougageKilos <= 10)
+            {
+                lougagePrice = 0.20 * priceLaugageOver20Kilos;
+            }
+            else if (lougageKilos <= 20)
+            {
+                lougagePrice = 0.50 * priceLaugageOver20Kilos;
+            }
+            else
+            {
+                double tax = double.Parse(Console.ReadLine());
+                lougagePrice = tax * priceLaugageOver20Kilos;
+            }
+            if (daysTillTrip > 30)
+            {
+                lougagePrice += 0.10 * lougagePrice;
+            }
+            else if (daysTillTrip <= 30 && daysTillTrip >= 7)
+            {
+                lougagePrice += 0.15 * lougagePrice;
+            }
+            else 
+            {
+                lougagePrice += 0.40 * lougagePrice;
+            }
+            double total = lougagePrice * lougageCount;
+            Console.WriteLine($"The total price of bags is: {total:f2} lv.");
+        }
+    }
+}
---
+++ T03. Aluminum Joinery/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace T03._Aluminum_Joinery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int joineries = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            string choice = Console.ReadLine();
+
+            double total = 0;
+
+            if (joineries <= 10)
+            {
+                Console.WriteLine("Invalid order");
+                return;
+            }
+            if (type == "90X130")
+            {
+                total = 110 * joineries;
+                if (joineries > 30 && joineries <= 60)
+                {
+                    total *= 0.95;
+                }
+                else if (joineries > 60)
+                {
+                    total *= 0.92;
+                }
+            }
+            else if (type == "100X150")
+            {
+                total = 140 * joineries;
+                if (joineries > 40 && joineries <= 80)
+                {
+                    total *= 0.94;
+                }
+                else if (joineries > 80)
+                {
+                    total *= 0.90;
+                }
+            }
+            else if (type == "130X180")
+            {
+                total = 190 * joineries;
+                if (joineries > 20 && joineries <= 50)
+                {
+                    total *= 0.93;
+                }
+                else if (joineries > 50)
+                {
+                    total *= 0.88;
+                }
+            }
+            else
+            {
+                total = 250 * joineries;
+                if (joineries > 25 && joineries <= 50)
+                {
+                    total *= 0.91;
+                }
+                else if (joineries > 50)
+                {
+                    total *= 0.86;
+                }
+            }
+            if (choice == "With delivery")
+            {
+                total += 60;
+            }
+            if (joineries > 99)
+            {
+                total *= 0.96;
+            }
+            Console.WriteLine($"{total:f2} BGN");
+        }
+    }
+}
