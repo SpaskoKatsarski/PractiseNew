9de0a8470d3def170bc8df632d9d3cb67b9de613
Spasko Katsarski <patsokatsarski1@gmail.com>
2021-10-14 21:06:14 +0300

    Updated

---
+++ T04. Balls/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace T04._Balls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double pointsFromRed = 0;
+            double pointsFromOrange = 0;
+            double pointsFromYellow = 0;
+            double pointsFromWhite = 0;
+            double black = 0;
+            double totalPoints = 0;
+            int otherColours = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string colour = Console.ReadLine();
+                if (colour == "red")
+                {
+                    pointsFromRed++;
+                    totalPoints += 5;
+                }
+                else if (colour == "orange")
+                {
+                    pointsFromOrange++;
+                    totalPoints += 10;
+                }
+                else if (colour == "yellow")
+                {
+                    pointsFromYellow++;
+                    totalPoints += 15;
+                }
+                else if (colour == "white")
+                {
+                    pointsFromWhite++;
+                    totalPoints += 20;
+                }
+                else if (colour == "black")
+                {
+                    black++;
+                    totalPoints = Math.Floor(totalPoints / 2);
+                }
+                else
+                {
+                    otherColours++;
+                }
+            }
+            Console.WriteLine($"Total points: {totalPoints}\nPoints from red balls: {pointsFromRed}\nPoints from orange balls: {pointsFromOrange}\nPoints from yellow balls: {pointsFromYellow}\nPoints from white balls: {pointsFromWhite}\nOther colors picked: {otherColours}\nDivides from black balls: {black}");
+        }
+    }
+}
---
+++ T05. Best Player/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace T05._Best_Player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentPlayer = String.Empty;
+            string bestPlayer = string.Empty;
+            int mostGoals = int.MinValue;
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                currentPlayer = input;
+                int markedGoals = int.Parse(Console.ReadLine());
+
+                if (markedGoals > mostGoals)
+                {
+                    bestPlayer = currentPlayer;
+                    mostGoals = markedGoals;
+
+                    if (mostGoals >= 10)
+                    {
+                        Console.WriteLine($"{bestPlayer} is the best player!\nHe has scored {mostGoals} goals and made a hat-trick !!!");
+                        return; 
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            if (mostGoals >= 3)
+            {
+                Console.WriteLine($"{bestPlayer} is the best player!\nHe has scored {mostGoals} goals and made a hat-trick !!!");
+            }
+            else
+            {
+                Console.WriteLine($"{bestPlayer} is the best player!\nHe has scored {mostGoals} goals.");
+            }
+        }
+    }
+}
